b67a28bbbaa30eef7eeaf8a071b272c03b6d69bd
gfsd3v <gfsd3v@gmail.com>
2025-01-19 18:19:56 +0100

    deleting core

--- pkg/core/interfaces.go
+++
@@ -1,8 +0,0 @@
-package core
-
-// Logger defines the interface for logging operations
-type Logger interface {
-	Debug(msg string, keysAndValues ...interface{})
-	Info(msg string, keysAndValues ...interface{})
-	Error(msg string, err error, keysAndValues ...interface{})
-}
